61d311ba515e5d06a53a806d8b177d23553f0c18
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-09-21 18:39:56 +0100

    Update HttpRequest method comment. We only need 3 methods

--- includes/HttpRequest.hpp
+++ includes/HttpRequest.hpp
@@ -5,7 +5,7 @@ class HttpRequest
 {
 private:
     // Request line components
-   std::string                         method;          // GET, POST, PUT, DELETE, etc.
+   std::string                         method;          // GET, POST, DELETE. We only need 3
    std::string                         uri;            // /path/to/resource
    std::string                         httpVersion;    // HTTP/1.1
    std::string                         root;           //path to root server
